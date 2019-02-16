9bdc8db6998b369ab757ab2aaee17d94dbb4879f
Alexander Moriarty <amoriarty@fetchrobotics.com>
2019-02-15 21:44:17 -0800

    Trimmed the branch to only the fetch_binary_drivers sub directory

--- fetch_system_config/root/opt/ros/roscore_poststart.bash
+++
@@ -1,9 +0,0 @@
-# This script is used by the roscore systemd service unit
-echo "waiting for roscore to come up"
-. /opt/ros/melodic/setup.sh
-ret=`rosnode list`
-while [ "$ret" = '' ]
-do
-    ret=`rosnode list`
-    sleep 1;
-done
