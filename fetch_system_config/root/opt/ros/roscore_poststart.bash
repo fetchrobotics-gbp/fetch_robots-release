6caaf32178d0e0e135c97e065c6f7d01368631a6
Eric Relson <erelson@fetchrobotics.com>
2021-03-05 22:02:15 -0800

    Trimmed the branch to only the fetch_bringup sub directory

--- fetch_system_config/root/opt/ros/roscore_poststart.bash
+++
@@ -1,9 +0,0 @@
-# This script is used by the roscore systemd service unit
-echo "waiting for roscore to come up"
-. /opt/ros/noetic/setup.sh
-ret=`rosnode list`
-while [ "$ret" = '' ]
-do
-    ret=`rosnode list`
-    sleep 1;
-done
